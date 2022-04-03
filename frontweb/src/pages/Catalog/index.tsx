1a6664e4327339e36962633946703fc27ea85289
Marcos Antonio Ribeiro <maribeiro9600@gmail.com>
2022-04-03 10:40:38 -0300

    Catalog listing

--- frontweb/src/pages/Catalog/index.tsx
+++ frontweb/src/pages/Catalog/index.tsx
@@ -1,62 +1,68 @@
+import axios from 'axios';
 import Pagination from 'components/Pagination';
 import ProductCard from 'components/ProductCard';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Product } from 'types/product';
+import { AxiosParams } from 'types/Vendor/axios';
+import { SpringPage } from 'types/Vendor/spring';
+import { isConstructorDeclaration } from 'typescript';
+import { BASE_URL } from 'util/requests';
 
 import './styles.css';
-
+/* 
 const product: Product = {
-  id: 2,
-  name: 'Smart TV',
-  description:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-  price: 2190.0,
-  imgUrl:
-    'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg',
-  date: '2020-07-14T10:00:00Z',
-  categories: [
-    {
-      id: 1,
-      name: 'Livros',
-    },
-    {
-      id: 3,
-      name: 'Computadores',
-    },
-  ],
-};
+  "id": 1,
+  "name": "The Lord of the Rings",
+  "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  "price": 90.5,
+  "imgUrl": "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg",
+  "date": "2020-07-13T20:50:07.123450Z",
+  "categories": [
+      {
+          "id": 2,
+          "name": "Eletrônicos"
+      }
+  ]
+} */
 
 const Catalog = () => {
+  const [page, setPage] = useState<SpringPage<Product>>();
+
+  useEffect(() => {
+    const params: AxiosParams = {
+      method: 'GET',
+      url: `${BASE_URL}/products`,
+      params: {
+        page: 0,
+        size: 12,
+      },
+    };
+    axios(params).then((response) => {
+      setPage(response.data);
+      console.log(page);
+    });
+  }, []);
+
   return (
     <div className="container my-4 catalog-container">
       <div className="row catalog-title-container">
         <h1>Catálogo de produtos</h1>
       </div>
+
       <div className="row">
-        <div className="col-sm-6 col-lg-4 col-xl-3">
-          <Link to="/products/1">
-            <ProductCard product={product} />
-          </Link>
-        </div>
-        <div className="col-sm-6 col-lg-4 col-xl-3">
-          <Link to="/products/1">
-            <ProductCard product={product} />
-          </Link>
-        </div>
-        <div className="col-sm-6 col-lg-4 col-xl-3">
-          <Link to="/products/1">
-            <ProductCard product={product} />
-          </Link>
-        </div>
-        <div className="col-sm-6 col-lg-4 col-xl-3">
-          <Link to="/products/1">
-            <ProductCard product={product} />
-          </Link>
-        </div>
+        {page?.content.map((product) => (
+          <div className="col-sm-6 col-lg-4 col-xl-3" key={product.id}>
+            <Link to="/products/1">
+              <ProductCard product={product} />
+            </Link>
+          </div>
+        ))}
       </div>
-      <div className='row'>
+
+      <div className="row">
         <Pagination />
-        </div>
+      </div>
     </div>
   );
 };
